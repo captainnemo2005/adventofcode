1cc910a883afb1857cab11268069864f3688695b
cptnemo2005 <kytringuyen95@gmail.com>
2022-06-13 13:50:30 +1000

    Solution for day 6

--- code/day5.py
+++ code/day5.py
@@ -9,18 +9,19 @@ def read_file() -> list:
     ) as file:
         lines = [line.split(" -> ") for line in file.read().strip().split("\n")]
     return [
-        [(int(s[0]),int(s[1])) for s in (x.split(",") for x in line)] for line in lines
+        [(int(s[0]), int(s[1])) for s in (x.split(",") for x in line)] for line in lines
     ]
 
-def count_crossings(coords:list, allow_diagonoal:bool = False) -> int:
+
+def count_crossings(coords: list, allow_diagonoal: bool = False) -> int:
     segments = dict()
     for coord in coords:
-        x1, x2, y1, y2 = coord[0][0],coord[1][0],coord[0][1],coord[1][1]
-        x_dir = 1 if x1<x2 else -1 if x1>x2 else 0
-        y_dir = 1 if y1<y2 else -1 if y1>y2 else 0
-        dist = max(abs(x2-x1),abs(y2-y1))
+        x1, x2, y1, y2 = coord[0][0], coord[1][0], coord[0][1], coord[1][1]
+        x_dir = 1 if x1 < x2 else -1 if x1 > x2 else 0
+        y_dir = 1 if y1 < y2 else -1 if y1 > y2 else 0
+        dist = max(abs(x2 - x1), abs(y2 - y1))
         cond = True if allow_diagonoal else (x_dir == 0 or y_dir == 0)
-        tmp = [(x1 + n * x_dir, y1 + n*y_dir) for n in range(dist + 1) if cond]
+        tmp = [(x1 + n * x_dir, y1 + n * y_dir) for n in range(dist + 1) if cond]
         for segment in tmp:
             if segment not in segments:
                 segments[segment] = 1
@@ -28,11 +29,14 @@ def count_crossings(coords:list, allow_diagonoal:bool = False) -> int:
                 segments[segment] += 1
     return sum(1 for x in segments if segments[x] > 1)
 
-def part1(coords:list) -> int:
+
+def part1(coords: list) -> int:
     return count_crossings(coords)
 
-def part2(coords:list) -> int:
-    return count_crossings(coords,True)
+
+def part2(coords: list) -> int:
+    return count_crossings(coords, True)
+
 
 if __name__ == "__main__":
     coords = read_file()
---
+++ code/day6.py
@@ -0,0 +1,53 @@
+"""
+Fish span 1 every 7 days
+Although you know nothing about this specific species of lanternfish,
+you make some guesses about their attributes. Surely, each lanternfish creates a new lanternfish once every 7 days.
+
+However, this process isn't necessarily synchronized between every lanternfish -
+one lanternfish might have 2 days left until it creates another lanternfish, while another might have 4.
+So, you can model each fish as a single number that represents the number of days until it creates a new lanternfish.
+
+Furthermore, you reason, a new lanternfish would surely need slightly longer before it's capable of producing more lanternfish: two more days for its first cycle.
+
+"""
+
+import numpy as np
+
+
+def read_data() -> list:
+    with open(
+        "/Users/cptnemo2005/Desktop/Work/self-sufficient-me/adventofcode/data/day6data.txt",
+        "r",
+    ) as file:
+        line = file.read().strip().split("\n")[0].split(",")
+    return [int(x) for x in line]
+
+
+def part1(fishes: list, day: int) -> int:
+    print("Initial state: ",fishes)
+    for i in range(0, day):
+        for j in range(len(fishes)):
+            if fishes[j] == 0:
+                fishes[j] = 6
+                fishes.append(8)
+            else:
+                fishes[j] -= 1
+        # if i == 0:
+        #     print("After day ", i+1, " day: ",fishes)
+        # else:
+        #     print("After day ", i + 1, " days: ",fishes)
+        print(len(fishes))
+    return len(fishes)
+
+def part2(fishes: list, day: int)-> int:
+    fishes_ = [fishes.count(x) for x in range(9)]
+    for i in range(day):
+        new_fishes = fishes_[0]
+        fishes_ = fishes_[1:] + [new_fishes]
+        fishes_[6] += new_fishes
+        print("Day ", i , " fishes = ", sum(fishes_))
+    return sum(fishes_)
+if __name__ == "__main__":
+    fishes = read_data()
+    # print(f"Part 1: {part1(fishes,80)}")
+    print(f"Part 2: {part2(fishes,256)}")
