c4526144e843c05c1f5661fb93e32635662b19e6
cptnemo2005 <kytringuyen95@gmail.com>
2022-06-26 22:47:22 +1000

    Finalize day 13

--- code/day13.py
+++ code/day13.py
@@ -45,68 +45,72 @@ def read_file():
         folds = []
 
         for line in lines:
-            if 'fold' in line:
-                folds.append(line.split()[-1].split('='))
+            if "fold" in line:
+                folds.append(line.split()[-1].split("="))
             elif len(line) > 0:
-                x,y = line.split(',')
+                x, y = line.split(",")
                 if int(x) > max_x:
                     max_x = int(x)
                 if int(y) > max_y:
                     max_y = int(y)
 
-        grid = [[' '] * (max_x + 1) for _ in range((max_y+1))]
+        grid = [[" "] * (max_x + 1) for _ in range((max_y + 1))]
 
         for line in lines:
-            if (len(line)>0) and ('fold' not in line):
-                y, x = line.split(',')
+            if (len(line) > 0) and ("fold" not in line):
+                y, x = line.split(",")
 
-                grid[int(x)][int(y)] = '#'
-        return folds,grid
+                grid[int(x)][int(y)] = "#"
+        return folds, grid
 
-def part1(folds,grid):
+
+def part1(folds, grid):
     point = 0
     val = int(folds[0][1])
-    if folds[0][0] == 'y':
+    if folds[0][0] == "y":
         for i in range(val):
             for j in range(len(grid[0])):
-                if grid[i][j] == '#' or grid[2*val - i][j] == '#':
+                if grid[i][j] == "#" or grid[2 * val - i][j] == "#":
                     point += 1
     else:
         for i in range(len(grid)):
             for j in range(val):
-                if grid[i][j] == '#' or grid[i][2*val - j] == '#':
+                if grid[i][j] == "#" or grid[i][2 * val - j] == "#":
                     point += 1
     return point
 
-def part2(folds,grid):
-    with open('../data/day13data.txt') as f:
+
+def part2(folds, grid):
+    with open("../data/day13data.txt") as f:
         lines = f.read().splitlines()
 
-    grid = [[' '] * 2000 for _ in range(2000)]
+    grid = [[" "] * 2000 for _ in range(2000)]
     folds = []
 
     for line in lines:
-        if 'fold' in line:
-            folds.append(line.split()[-1].split('='))
+        if "fold" in line:
+            folds.append(line.split()[-1].split("="))
         elif len(line) > 0:
-            x, y = line.split(',')
-            grid[int(x)][int(y)] = '#'
+            x, y = line.split(",")
+            grid[int(x)][int(y)] = "#"
 
     for f in folds:
         val = int(f[1])
-        if f[0] == 'x':
+        if f[0] == "x":
             for i in range(val):
                 for a in range(2000):
-                    if grid[i][a] == '#' or grid[2 * val - i][a] == '#':
-                        grid[i][a] = '#'
+                    if grid[i][a] == "#" or grid[2 * val - i][a] == "#":
+                        grid[i][a] = "#"
         else:
             for i in range(2000):
                 for a in range(val):
-                    if grid[i][a] == '#' or grid[i][2 * val - a] == '#':
-                        grid[i][a] = '#'
+                    if grid[i][a] == "#" or grid[i][2 * val - a] == "#":
+                        grid[i][a] = "#"
 
     for i in range(40):
-        print(''.join(grid[i][:6][::-1]))
+        print("".join(grid[i][:6][::-1]))
+
+
 if __name__ == "__main__":
     folds, grid = read_file()
     log.info(f"Part 1: {part1(folds,grid)}")
